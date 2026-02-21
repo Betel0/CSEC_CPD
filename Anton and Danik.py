6aea2cd0e9e8b15e0ac4bceb87853e50b1bcbe22
Betel Habtemariam <betelmamush19@gmail.com>
2026-02-21 22:36:40 +0300

    Add Anton and Danik game logic

---
+++ Anton and Danik.py
@@ -0,0 +1,18 @@
+n = int(input())
+s = input()
+
+anton = 0
+danik = 0
+
+for ch in s:
+    if ch == 'A':
+        anton += 1
+    else:
+        danik += 1
+
+if anton > danik:
+    print("Anton")
+elif danik > anton:
+    print("Danik")
+else:
+    print("Friendship")
